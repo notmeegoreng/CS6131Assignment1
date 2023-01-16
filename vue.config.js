1d283fcae6b9a11d619caad178047dc6f64e82ab
notmeegoreng <notmeegoreng@gmail.com>
2023-01-16 14:29:53 +0800

    try exporting

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from '@vue/cli-service'
-defineConfig({
+
+export default defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
+  devServer: {
+    port: 1212
+  },
   publicPath: process.env.NODE_ENV === 'production' ? '/CS6131Assignment1/' : '/'
 })
