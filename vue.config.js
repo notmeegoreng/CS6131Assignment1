67540efd413cae6e2b0e17ace7f1eab92d044a3e
notmeegoreng <notmeegoreng@gmail.com>
2023-01-16 09:22:03 +0800

    config reponame

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from '@vue/cli-service'
 defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === 'production' ? '/CS6131Assignment1/' : '/'
 })
